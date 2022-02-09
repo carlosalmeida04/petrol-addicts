90bd02643ee852f3c9040614e979f98e50c8546d
Carlos Almeida <59896120+carlosalmeida04@users.noreply.github.com>
2022-02-09 16:32:52 +0000

    Expo statusbar

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
 
     return (
         <>
-            <StatusBar barStyle="dark-content" />
+            <StatusBar style='auto' />
             <StackProvider />
         </>
 
