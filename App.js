d66762027608607e75dce98f394a4f839b0a74f6
Carlos Almeida <59896120+carlosalmeida04@users.noreply.github.com>
2022-03-04 23:34:00 +0000

    COMMIT DE BACKUP 04-03-2022

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-
+///
 LogBox.ignoreAllLogs()
 
 export default function App() {
