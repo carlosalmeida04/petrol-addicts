eefeb3d405a3082b0883f0df564b06bbca10dd27
Carlos Almeida <59896120+carlosalmeida04@users.noreply.github.com>
2022-03-05 01:46:08 +0000

    .

--- screens/components/OverflowMenu.js
+++ screens/components/OverflowMenu.js
@@ -8,6 +8,7 @@ import { deletePost } from './Reducers'
 export default function OverflowMenuButton({ postId }) {
 
     const [visible, setVisible] = useState(false)
+    
     const RenderButton = () => (
         <TouchableOpacity onPress={() => setVisible(true)}>
             <Icon name={"more-vertical-outline"} fill="black" style={{ width: 20, height: 20 }} />
