b835cf0509a76c530b44ab4630e722e2391f0df0
Carlos Almeida <59896120+carlosalmeida04@users.noreply.github.com>
2022-03-10 15:08:13 +0000

    Testing modal

--- screens/feed/Search.js
+++ screens/feed/Search.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, View, Dimensions } from 'react-native'
 
-import { Text } from "@ui-kitten/components"
+import Modal from "react-native-modal"
+
+import { Text, Button } from "@ui-kitten/components"
 
 export default function Search() {
 
+    const [open, setOpen] = React.useState(false)
+
     return (
-        <SafeAreaView style={{ backgroundColor: "#fff", flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Search!</Text>
-        </SafeAreaView>
+        <>
+            <SafeAreaView style={{ backgroundColor: "#fff", flex: 1, justifyContent: "center", alignItems: "center" }}>
+
+
+
+
+
+                <Button onPress={() => setOpen(true)}>Open modal</Button>
+            </SafeAreaView>
+            <Modal isVisible={open}
+
+            >
+                <View style={{ flex: 1, backgroundColor: "#eee" }}>
+                    <Text>I am the modal content!</Text>
+                    <Button onPress={() => setOpen(false)}>Fechar</Button>
+                </View>
+            </Modal>
+        </>
     )
 }
 
