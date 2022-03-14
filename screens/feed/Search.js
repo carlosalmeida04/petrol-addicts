8730e1a2e5798fb8fa6be971c6b0e4d3829e3fb0
Carlos Almeida <59896120+carlosalmeida04@users.noreply.github.com>
2022-03-14 14:12:58 +0000

    Search needs to start

--- screens/feed/Search.js
+++ screens/feed/Search.js
@@ -1,33 +1,15 @@
 import React from 'react'
-import { SafeAreaView, View, Dimensions } from 'react-native'
+import { SafeAreaView } from 'react-native'
 
-import Modal from "react-native-modal"
-
-import { Text, Button } from "@ui-kitten/components"
+import { Text } from "@ui-kitten/components"
 
 export default function Search() {
 
-    const [open, setOpen] = React.useState(false)
-
     return (
-        <>
-            <SafeAreaView style={{ backgroundColor: "#fff", flex: 1, justifyContent: "center", alignItems: "center" }}>
 
-
-
-
-
-                <Button onPress={() => setOpen(true)}>Open modal</Button>
-            </SafeAreaView>
-            <Modal isVisible={open}
-
-            >
-                <View style={{ flex: 1, backgroundColor: "#eee" }}>
-                    <Text>I am the modal content!</Text>
-                    <Button onPress={() => setOpen(false)}>Fechar</Button>
-                </View>
-            </Modal>
-        </>
+        <SafeAreaView style={{ backgroundColor: "#fff", flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Pesquisar</Text >
+        </SafeAreaView>
     )
 }
 
