ef255df0eec64cfde0dc22be5068af5d7b56703d
Carlos Almeida <59896120+carlosalmeida04@users.noreply.github.com>
2022-03-15 21:46:30 +0000

    Still remaking Create post

--- screens/feed/posts/create/pages/CarInfo.js
+++ screens/feed/posts/create/pages/CarInfo.js
@@ -5,9 +5,14 @@ import Header from '../../../../components/Header'
 export default function CarInfo({ navigation, route }) {
 
     const params = route.params
+
+    function navigateNextScreen() {
+        navigation.navigate("Overview")
+    }
+
     return (
         <View>
-            <Header buttonOnPress={() => navigation.navigate("Overview")} buttonText={"Seguinte"} title="Informações do carro" />
+            <Header buttonOnPress={navigateNextScreen} buttonText={"Seguinte"} title="Informações do carro" />
             <Text>{params.car}</Text>
         </View>
     )
